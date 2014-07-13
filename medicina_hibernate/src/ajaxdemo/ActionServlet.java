41b2240d142a7ba93d4b9aa90a4e4fd0a598df2a
EnricoGiordano1992 <enricogiordano1992@gmail.com>
2014-07-14 01:28:26 +0200

    aggiunta miglioria ajax

--- medicina_hibernate/src/ajaxdemo/ActionServlet.java
+++ medicina_hibernate/src/ajaxdemo/ActionServlet.java
@@ -37,7 +37,7 @@ public class ActionServlet extends HttpServlet {
 		ArrayList<CartellaClinica> cartelle = dbms.getCartelleDelPaziente(paziente);
 
 		for(int i = 0; i < cartelle.size(); i++)
-			c.put(cartelle.get(i).getId(), cartelle.get(i).getId());
+			c.put(cartelle.get(i).getId(), cartelle.get(i).getId() + " ( " + cartelle.get(i).getDataRicovero() + " <> " + cartelle.get(i).getDataDimissione() + " )");
 
 		String json = null ;
 		json= new Gson().toJson(c);   
